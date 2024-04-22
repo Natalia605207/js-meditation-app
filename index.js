88404acb335f84b542fa8fca6166166845bc7eb3
Natalia605207 <natalyaobukhova87@gmail.com>
2024-04-22 23:08:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,2 @@
+gsap.from("#header", {delay: 0.7, duration: 2.5, y: 150, ease: "power1.out", opacity: 0})
+gsap.from("#btn-container", {delay: 2.5, duration: 2.5, scale: 1, ease: "power2.inOut", opacity: 0})
